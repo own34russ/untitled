6738f9000c69b2c3fc58cb100b9e3e3e286a6873
own34russ <own3r34@mail.ru>
2021-04-22 16:01:18 +0300

    done

--- src/main/kotlin/Zadachi/codforces/4a.kt
+++ src/main/kotlin/Zadachi/codforces/4a.kt
@@ -1,10 +1,9 @@
 package Zadachi.codforces
-fun main(args:Array<String>) {
 
-    val weight =  readLine()!!.toInt()
-
-    if ((weight % 2)==0){
-        println("YES")}
-    else {
+fun main(args: Array<String>){
+    val weight = readLine()!!.toInt();
+    if (weight>2 && weight%2==0)
+        println("YES")
+    else
         println("NO")
-    }}
+}
