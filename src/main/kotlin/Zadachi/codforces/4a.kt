4bf9afdae5ff4d713e6e49035b1e924469d97185
own34russ <own3r34@mail.ru>
2021-04-23 11:11:29 +0300

    Revert "Упрощение (return if)"

--- src/main/kotlin/Zadachi/codforces/4a.kt
+++ src/main/kotlin/Zadachi/codforces/4a.kt
@@ -2,5 +2,8 @@ package Zadachi.codforces
 
 fun main(args: Array<String>){
     val weight = readLine()!!.toInt();
-    println( if (weight > 2 && weight % 2 == 0) "YES" else "NO" );
+    if (weight>2 && weight%2==0)
+        println("YES")
+    else
+        println("NO")
 }
