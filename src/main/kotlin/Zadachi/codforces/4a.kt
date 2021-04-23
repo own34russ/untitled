fc8d0a863c0f1fd54e26d5a71577ffe68cd86a44
Aleksei <54249358+Moonspeaker@users.noreply.github.com>
2021-04-23 10:20:30 +0400

    Update 4a.kt

--- src/main/kotlin/Zadachi/codforces/4a.kt
+++ src/main/kotlin/Zadachi/codforces/4a.kt
@@ -2,5 +2,5 @@ package Zadachi.codforces
 
 fun main(args: Array<String>){
     val weight = readLine()!!.toInt();
-    println( if (weight>2 && weight%2==0) "YES" else "NO" );
+    println( if (weight > 2 && weight % 2 == 0) "YES" else "NO" );
 }
